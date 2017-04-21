e0174b774cc693cc235db39bf62ef6cb2c98a996
Fabio Hiroki <fabiothiroki@gmail.com>
2017-04-21 17:12:18 -0300

    Rewritten in rxjs

--- src/app.js
+++ src/app.js
@@ -3,10 +3,62 @@ import { Observable } from 'rxjs'
 
 export function App (sources) {
 
+  const intents$ = {
+    apiResponse: sources.HTTP.select('api').flatten(),
+
+    changeSearchTerm: sources.DOM.select('#search')
+      .events("input")
+      .map(ev => ev.target.value)
+      .startWith('')
+  }
+
+  const state$ = state(intents$)
+
   return {
-    DOM: Observable.of(div("Hello World!")),
+    DOM: view(state$),
+    HTTP: intents$.changeSearchTerm.map( searchTerm => {
+      return {
+        url: 'https://swapi.co/api/people/?search=' + searchTerm,
+        category: 'api',
+      }
+    })
   };
 }
 
+function state(intents$){
+
+  return Observable.combineLatest(intents$.apiResponse, intents$.changeSearchTerm)
+    .map((combined) => {
+
+      const [response, searchTerm] = combined
+
+      return {
+        characters: response.body.results,
+
+        searchTerm: searchTerm
+      };
+    })
+    .startWith({
+      characters: [{name: 'Loading'}],
+
+      searchTerm: ''
+    })
+}
 
 
+function view(state$) {  
+  return state$.map((state) => {
+
+    console.log(state.searchTerm);
+
+    const list = state.characters.map( character => {
+      return p(character.name);
+    });
+
+    return div([
+      input("#search", {type: "text", value: state.searchTerm, autocomplete: "off"}),
+      div(list)
+   ]);
+
+  });  
+}
