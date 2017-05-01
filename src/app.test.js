76eb6f47be95b9c408ef0bf75bfe91462eac08d7
Fabio Hiroki <fabiothiroki@gmail.com>
2017-05-01 13:42:15 -0300

    Valid tests

--- src/app.test.js
+++ src/app.test.js
@@ -1,9 +1,9 @@
 import {div, p, input, h1, h2, tr, td, tbody, thead, th, table, h4, h5} from '@cycle/dom';
-import assert from 'assert';
 import {view} from './view';
-import { Observable } from 'rxjs';
+import {Observable} from 'rxjs';
 var chai = require('chai');
 chai.use(require('chai-virtual-dom'));
+var expect = chai.expect;
 var h = require('virtual-dom').h;
 
 
@@ -14,7 +14,7 @@ describe('View tests', function () {
     const state$ = getInitialState();
     const view$ = view(state$);
 
-    assert.notEqual(view$, null);
+    expect(view$).to.not.be.null;
 
   });
 
@@ -31,18 +31,17 @@ describe('View tests', function () {
       div('.card-content .table-responsive',[
         table('.table', [
           thead(tr(th(h5('Name')))),
-          tbody(tr(td('Loading...')))
+          tbody([tr(td('Loading...'))])
         ])
       ])
     ]);
 
-    assert.deepEqual(view$. expected$);
-
+    expect(view$).to.look.exactly.like(expected$);
   });
 
   it('Should populate character data', function () {
 
-    const state$ = getInitialState();
+    const state$ = getValidState();
     const view$ = view(state$);
 
     const expected$ = div(".card", [
@@ -53,13 +52,15 @@ describe('View tests', function () {
       div('.card-content .table-responsive',[
         table('.table', [
           thead(tr(th(h5('Name')))),
-          tbody(tr(td('Loading...')))
+          tbody([
+            tr(td('Darth Vader')),
+            tr(td('Darth Maul')),
+          ])
         ])
       ])
     ]);
 
-    assert.deepEqual(view$. expected$);
-
+    expect(view$).to.look.exactly.like(expected$);
   });
 
 })
@@ -75,8 +76,8 @@ function getInitialState() {
 
 function getValidState() {
   const state$ = Observable.of({
-      characters: [{name: 'Loading...'}],
-      searchTerm: ''
+      characters: [{name: 'Darth Vader'}, {name: 'Darth Maul'}],
+      searchTerm: 'darth'
   });
 
   return state$;
