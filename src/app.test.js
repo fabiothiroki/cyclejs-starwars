b64151f02bb9bee16fa10af7035376a11cf0e6fc
Fabio Hiroki <fabiothiroki@gmail.com>
2017-04-30 21:40:58 -0300

    Green tests

--- src/app.test.js
+++ src/app.test.js
@@ -1,3 +1,4 @@
+import {div, p, input, h1, h2, tr, td, tbody, thead, th, table, h4, h5} from '@cycle/dom';
 import assert from 'assert';
 import {view} from './view';
 import { Observable } from 'rxjs';
@@ -8,16 +9,32 @@ var h = require('virtual-dom').h;
 
 
 describe('View tests', function () {
+
   it('Should not be null', function () {
 
-    const state$ = Observable.of({
-      characters: [{name: 'Loading...'}],
-      searchTerm: ''
-    });
-
+    const state$ = getInitialState();
     const view$ = view(state$);
 
     expect(view$).to.not.be.null;
 
-  })
+  });
+
+  it('Should have loading state', function () {
+
+    const state$ = getInitialState();
+    const view$ = view(state$);
+
+    expect(view$).to.equal(view$);
+
+  });
+
 })
+
+function getInitialState() {
+  const state$ = Observable.of({
+      characters: [{name: 'Loading...'}],
+      searchTerm: ''
+  });
+
+  return state$;
+}
